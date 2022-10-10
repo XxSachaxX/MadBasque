9a254e039593b1f68341970c55a0f616e65ea636
Sacha Gotainer <sgotainer@hotmail.com>
2022-10-10 14:34:57 +0200

    Added basic routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-
+  get "concerts", to: "pages#concert"
+  get "tournages", to: "pages#tournage"
+  get "artistiques", to: "pages#artistique"
 end
