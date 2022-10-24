552e3c5fab2fc91a8c5ebdac67b85b8e8ad9a740
Sacha Gotainer <sgotainer@hotmail.com>
2022-10-24 18:01:29 +0200

    Updated stimulus controller and concerts index to enable toggling of d-none on it

--- app/javascript/controllers/disable_button_controller.js
+++ app/javascript/controllers/disable_button_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="disable-button"
 
 export default class extends Controller {
-  static targets = ["aura", "hail", "feux"]
+  static targets = ["aura", "hail", "feux", "stone", "yetili"]
 
   connect() {
     console.log("disabler connected")
@@ -25,5 +25,17 @@ export default class extends Controller {
     event.preventDefault()
     console.log("It works!!")
     this.feuxTarget.classList.toggle("d-none")
+  }
+
+  toggleStone() {
+    event.preventDefault()
+    console.log("It works!!")
+    this.stoneTarget.classList.toggle("d-none")
+  }
+
+  toggleYetili() {
+    event.preventDefault()
+    console.log("It works!!")
+    this.yetiliTarget.classList.toggle("d-none")
   }
 }
