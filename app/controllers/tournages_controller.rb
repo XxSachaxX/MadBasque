1b0692419e5ec856c9f4d879d71f7830f228fa57
Sacha Gotainer <sgotainer@hotmail.com>
2022-11-03 16:45:25 +0100

    Added banners for artistiques photo categories + added all photos + connected to stimuls controller to toggle display

--- app/controllers/tournages_controller.rb
+++ app/controllers/tournages_controller.rb
@@ -1,5 +1,6 @@
 class TournagesController < ApplicationController
   skip_before_action :authenticate_user!
+
   def index
     @tournages = Tournage.all
   end
--- app/javascript/controllers/disable_button_controller.js
+++ app/javascript/controllers/disable_button_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="disable-button"
 
 export default class extends Controller {
-  static targets = ["aura", "hail", "feux", "stone", "yetili"]
+  static targets = ["aura", "hail", "feux", "stone", "yetili", "abstrait", "kodak", "portraits", "street", "architecture", "paysages", "textures"]
 
   connect() {
     console.log("disabler connected")
@@ -37,5 +37,47 @@ export default class extends Controller {
     event.preventDefault()
     console.log("It works!!")
     this.yetiliTarget.classList.toggle("d-none")
+  }
+
+  toggleAbstrait() {
+    event.preventDefault()
+    console.log("It works!!")
+    this.abstraitTarget.classList.toggle("d-none")
+  }
+
+  toggleKodak() {
+    event.preventDefault()
+    console.log("It works!!")
+    this.kodakTarget.classList.toggle("d-none")
+  }
+
+  togglePortraits() {
+    event.preventDefault()
+    console.log("It works!!")
+    this.portraitsTarget.classList.toggle("d-none")
+  }
+
+  toggleStreet() {
+    event.preventDefault()
+    console.log("It works!!")
+    this.streetTarget.classList.toggle("d-none")
+  }
+
+  toggleArchitecture() {
+    event.preventDefault()
+    console.log("It works!!")
+    this.architectureTarget.classList.toggle("d-none")
+  }
+
+  togglePaysages() {
+    event.preventDefault()
+    console.log("It works!!")
+    this.paysagesTarget.classList.toggle("d-none")
+  }
+
+  toggleTextures() {
+    event.preventDefault()
+    console.log("It works!!")
+    this.texturesTarget.classList.toggle("d-none")
   }
 }
